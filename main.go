dc9d5a3463257a1a656e5a19b744f290170ac089
Pranoy Kumar Kundu <pranoy1998k@gmail.com>
2025-02-02 22:50:11 +0530

    set host to connection based on the url scheme from the host

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"log"
 	"net"
+	"net/url"
 	"os"
 	"time"
 )
@@ -15,30 +16,45 @@ import (
 func main() {
 	host := os.Getenv("HOST")
 	port := os.Getenv("PORT")
-	// username := os.Getenv("USERNAME")
-	// password := os.Getenv("PASSWORD")
+
+	addr, err := url.Parse(host)
+	if err != nil {
+		log.Fatalf("error occurred while parsing host, error: %v", err)
+	}
+	isPlainTCP := false
+	if addr.Scheme == "" {
+		isPlainTCP = true
+	}
 
 	// set net connecction
-	netConn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%s", host, port), time.Second*15)
+	var parsedHost string
+	if isPlainTCP {
+		parsedHost = host
+	} else {
+		parsedHost = addr.Host
+	}
+	netConn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%s", parsedHost, port), time.Second*15)
 	if err != nil {
 		log.Fatalf("error occurred while setting up net connection, error: %v", err)
 	}
 	defer netConn.Close()
 
-	tlsConfig := &tls.Config{
-		ServerName:         host,
-		InsecureSkipVerify: true,
+	finalConn := netConn
+	if !isPlainTCP {
+		tlsConfig := &tls.Config{
+			ServerName:         host,
+			InsecureSkipVerify: true,
+		}
+
+		// setup client side connection and it returns a greeting which is parsed in the next step
+		finalConn := tls.Client(netConn, tlsConfig)
+		defer finalConn.Close()
 	}
-
-	// setup client side connection and it returns a greeting which is parsed in the next step
-	conn := tls.Client(netConn, tlsConfig)
-	defer conn.Close()
-
 	// read greeting
 	var buf bytes.Buffer
 	var header int32
 
-	err = binary.Read(conn, binary.BigEndian, &header)
+	err = binary.Read(finalConn, binary.BigEndian, &header)
 	if err != nil {
 		log.Fatalf("error while reading bytes from the connection, error: %v", err)
 	}
@@ -50,7 +66,7 @@ func main() {
 	msgLen := int64(header - 4)
 
 	// reads the bytes from conn upto the msgLen. Returns when EOF is returned or msgLen is reached
-	lr := io.LimitReader(conn, msgLen)
+	lr := io.LimitReader(finalConn, msgLen)
 
 	// reads from lr to buf
 	_, err = buf.ReadFrom(lr)
